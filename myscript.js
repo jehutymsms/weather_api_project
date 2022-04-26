fcf1307565501f88150c9f0e2fd5154fc921997b
Andrew Humphries <andrewhumphrieswork@outlook.com>
2022-04-26 06:17:13 -0500

    removed giphy api

--- myscript.js
+++ myscript.js
@@ -18,12 +18,13 @@ async function getWeather(){
 
     const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${defaultSearch}&limit=1&appid=0c32baf96cab07598f9ffa6ec5612d52`),
         weatherData = await response.json(),
-        response2 = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${weatherData[0].lat}&lon=${weatherData[0].lon}&appid=0c32baf96cab07598f9ffa6ec5612d52`),
+        response2 = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${weatherData[0].lat}&lon=${weatherData[0].lon}&appid=0c32baf96cab07598f9ffa6ec5612d52&units=imperial`),
         preciseWeatherData = await response2.json();
 
     console.log(weatherData[0].lat)
     console.log(weatherData[0].lon)
     console.log(preciseWeatherData)
+
     
 
     }catch(err){
@@ -37,18 +38,4 @@ button.addEventListener('click', function(event){
 });
 
 
-
-async function getWeatherImage(defaultSearch = 'sunny'){
-    try{
-    const response = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Re8JS3jj5HnRyaC0Gs1odAg1cxCjXmvP&s=${defaultSearch}`),
-        weatherImage = await response.json();
-
-    body.style.backgroundImage = weatherImage.data.images.original.url
-    }catch(err){
-        console.log('search did not return anything')
-    }
-};
-
-
 console.log(mainDisplayItem.lastElementChild.innerHTML)
-getWeatherImage()
